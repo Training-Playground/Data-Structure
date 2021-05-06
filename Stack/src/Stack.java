33ee7ccc936e0f11cd79491b2add3432217ce0c7
Hamza Nazli <chemhamza@gmail.com>
2021-05-06 11:21:08 +0530

    //

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,9 +1,20 @@
-public class Stack {
-    public void push(int number) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Stack {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int arrayNum = input.nextInt();
+        push
+    }
+    public ArrayList numbersArray = new ArrayList();
+    public void push(int number) {
+        numbersArray.add(number);
     }
     public void pop(){
-
+        numbersArray.remove(numbersArray.toArray().length-1);
     }
     public boolean empty(){
         return true;
@@ -14,9 +25,13 @@ public class Stack {
     }
 
     public void print(){
-        System.out.println();
+        System.out.println(Arrays.toString(numbersArray));
     }
+
+
+
     public boolean contains(int number){
+
         return true;
     }
 }
