6324419d85940a500be89e848f6a874696ccbf17
Hamza Nazli <chemhamza@gmail.com>
2021-05-06 11:00:49 +0530

    methods created(contents not added)

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,2 +1,22 @@
 public class Stack {
+    public void push(int number) {
+
+    }
+    public void pop(){
+
+    }
+    public boolean empty(){
+        return true;
+    }
+    public int peek(){
+        int num=0;
+        return num;
+    }
+
+    public void print(){
+        System.out.println();
+    }
+    public boolean contains(int number){
+        return true;
+    }
 }
