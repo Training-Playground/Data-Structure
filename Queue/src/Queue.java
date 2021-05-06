99c2dbe4486d44066ad7b7f1c1928fee0678e521
Hamza Nazli <chemhamza@gmail.com>
2021-05-06 14:33:29 +0530

    Methods for Queue Class added

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,3 +1,26 @@
 public class Queue {
+    public void enqueue(int number){
 
+    }
+    public void dequeue(){
+
+    }
+    public boolean empty(){
+        return false;
+    }
+    public int peek(){
+        return 0;
+    }
+    public void clear(){
+
+    }
+    public void print(){
+
+    }
+    public boolean contains(int number){
+        return false;
+    }
+    public int size(){
+        return 0;
+    }
 }
