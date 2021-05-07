34d8ef54119fc6911df086c029a1665d1e631e59
Hamza Nazli <chemhamza@gmail.com>
2021-05-07 11:47:56 +0530

    DynamicArray Class Structure done

--- DynamicArray/src/ds3/DynamicArray.java
+++ DynamicArray/src/ds3/DynamicArray.java
@@ -1,5 +1,31 @@
 package ds3;
 
 public class DynamicArray {
+    public void add(int number){
 
+    }
+    public void add(int index,int number){
+
+    }
+    public void remove(int index){
+
+    }
+    public int get(int index){
+        return 0;
+    }
+    public void print(){
+
+    }
+    public void clear(){
+
+    }
+    public int size(){
+        return 0;
+    }
+    public boolean contains(int number){
+        return false;
+    }
+    public void empty(){
+
+    }
 }
